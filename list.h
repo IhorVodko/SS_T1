413cbf8f81972a940fafa382aee7df74fd187665
Ihor Vodko <ihorfinance@gmail.com>
2021-01-22 14:35:44 +0200

    fixed variant<>

--- list.h
+++ list.h
@@ -1,33 +1,54 @@
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <variant>
+#define ToInt GetValue<int>
+#define ToUInt GetValue<unsigned int>
+#define ToLInt GetValue<long int>
+#define ToFloat GetValue<float>
+#define ToDouble GetValue<double>
+#define ToLDouble GetValue<long double>
+#define ToChar GetValue<char>
+#define ToBool GetValue<bool>
+using namespace std;
+//safe to use form command line interface
 
-typedef char* String;
+enum Type {
+    TypeBegin = -1,
+    TypeInt, TypeUnsignedInt, TypeLongInt, TypeFloat,
+    TypeDouble, TypeLongDouble, TypeChar, TypeBool,
+    TypeEnd
+};
 
-/*--------------------------user interface functions--------------------------*/
-//initializes list
-void StringListInit(char*** headNode, char* str);
-//destroy list and set pointer to NULL
-void StringListDestroy(char*** headNode);
-//inserts value at the end of the list
-void StringListAdd(char** headNode, String str);
-//removes all occurrences of str in the list
-void StringListRemove(char*** headNode, char* str);
-//returns the number of items in the list
-size_t StringListSize(char** headNode);
-//returns the index position of the first occurrence of str in the list
-int StringListIndexOf(char** headNode, char* str);
-//removes duplicate entries from the list
-void StringListRemoveDuplicates(char** headNode);
-//replaces every occurrence of the "before", in each of the string list's strings, with "after"
-void StringListReplaceInStrings(char** headNode, char* before, char* after);
-//sorts the list of strings in ascending order
-void StringListSort(char** headNode);
-/*--------------------------utility functions--------------------------*/
-//returns string length
-int StrLen(const char* str);
-//compare strings, if match returns 0, otherwise ASCII dif
-int StrComp(const char* str1, const char* str2);
-//print list
-void StrListPrint(char** headNode);
+//the class template std::variant represents a type-safe union
+using FundamentalTypes = variant<int, unsigned int, long int, float, double, long double,
+                                char, bool>;
+/*class stores single values of cpp fundamental types
+and provides a tye-safe access to the contained value*/
+class AnyType {
+private:
+    FundamentalTypes myData;
+    const int GetEnumIndex(const string&) const;
+public:
+    void Swap(AnyType&);
+    template<typename Type>
+    Type GetValue() const;
+    string GetType() const;
+    AnyType() = default;
+    AnyType(FundamentalTypes data);
+//   prevent a class from constructing an object from a string, only char is allowed
+    AnyType(string) = delete;
+    //copy constructor
+    AnyType(const AnyType&);
+    template<typename Type>
+    //copy assignment
+    AnyType& operator=(const Type&);
+    AnyType& operator=(const AnyType&);
+    ~AnyType() = default;
+};
+
+//Fix type_info::name method
+const string FixTypeidName(const string& typeName);
+
+#include "AnyType.inl"
